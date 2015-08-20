89b0581536de81f980b994084d91376d71019831
vaspoz <vaspoz@mail.ru>
2015-08-20 14:11:38 +0300

    commit

--- src/main/java/ru/home/edu/mongodb/App.java
+++ src/main/java/ru/home/edu/mongodb/App.java
@@ -1,13 +1,38 @@
 package ru.home.edu.mongodb;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import com.mongodb.*;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
+        DB db = client.getDB("course");
+
+        DBCollection coll = db.getCollection("students");
+
+        coll.drop();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                coll.insert(new BasicDBObject("i", i).append("j", j));
+            }
+        }
+
+        Bson sort = new Document("i", -1);
+
+        DBCursor cur = coll.find()
+                .sort(new BasicDBObject("i", -1))
+//                .sort(new BasicDBObject("j", -1)
+        ;
+        show(cur);
+
+    }
+
+    private static void show(DBCursor cursor) {
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+        }
     }
 }
